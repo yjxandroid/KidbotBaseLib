0bed7f948cce6f6cd2af94243a4e3db65f090884
yjx <18602590523@163.com>
2015-11-07 22:39:55 +0800

    添加方法

--- Library/src/main/java/com/kidbot/library/utils/ScreenUtils.java
+++ Library/src/main/java/com/kidbot/library/utils/ScreenUtils.java
@@ -92,10 +92,41 @@ public final class ScreenUtils {
         return result;
     }
 
+    /**
+     * dp 转 px
+     * @param context 上下文
+     * @param dp dp值
+     * @return px值
+     */
+    public static int  dp2px(Context context,int dp){
+        float density = getDensity(context);
+        return (int)(dp * density + 0.5);
+    }
 
 
+    /**
+     * px 转 dp
+     * @param context 上下文
+     * @param px dp值
+     * @return dp值
+     */
+    public static int px2dp(Context context,int px){
+        float density = getDensity(context);
+        return (int)((px - 0.5) / density);
+    }
 
 
+    /**
+     * 得到屏幕分辨率
+     * @param context 上下文
+     * @return 分辨率
+     */
+    public static float getDensity(Context context){
+        WindowManager windowManager= (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics=new DisplayMetrics();
+         windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.density;
+    }
 
 
 
